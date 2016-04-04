cf78a95d22d0be189a78281a89149882f85f1f83
dchuan <chuan.dong@amadeus.com>
2016-04-04 20:53:54 +0200

    Hello world test

---
+++ src/main/java/com/dong/demo/SpELTest.java
@@ -0,0 +1,21 @@
+package com.dong.demo;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class SpELTest {
+
+	public static void main(String[] args) {
+
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression expression = parser.parseExpression("('Hello' + 'World').concat(#end)");
+		EvaluationContext context = new StandardEvaluationContext();
+		context.setVariable("end", "!");
+		System.out.println(expression.getValue(context));
+
+	}
+
+}
